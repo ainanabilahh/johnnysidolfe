0a4a5b22d5ce43143607464b53008383868cf605
ainanabilahh <ainanabilahh@gmail.com>
2021-02-25 14:31:11 +0800

    Init

---
+++ lib/hello_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+    runApp(SecondRoute());
+}
+  class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Johnny\'s Entertainment',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see theb
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.purple,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Johnny\'s Entertainment'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({
+    Key key,
+    this.title
+  }): super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State < MyHomePage > {
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: 2,
+            children: < Widget > [
+              GestureDetector(
+                child: Image(image: AssetImage("images/arashi.png")),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => DetailScreen("images/arashi.png"),
+                  ));
+                },
+              ),
+              GestureDetector(
+                child: Image(image: AssetImage("images/kinpuri.jpg")),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => DetailScreen("images/kinpuri.jpg"),
+                  ));
+                },
+              ),
+              GestureDetector(
+                child: Image(image: AssetImage("images/sixtones.jpg")),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => DetailScreen("images/sixtones.jpg"),
+                  ));
+                },
+              ),
+              GestureDetector(
+                child: Image(image: AssetImage("images/hsj.jpg")),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => DetailScreen("images/hsj.jpg"),
+                  ));
+                },
+              ),
+              GestureDetector(
+                child: Image(image: AssetImage("images/sexyzone.jpg")),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => DetailScreen("images/sexyzone.jpg"),
+                  ));
+                },
+              ),
+              GestureDetector(
+                child: Image(image: AssetImage("images/snowman.png")),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => DetailScreen("images/snowman.png"),
+                  ));
+                },
+              ),
+            ],
+        ),
+
+      ),
+    );
+  }
+}
+
+
+class DetailScreen extends StatelessWidget {
+  String something;
+  DetailScreen(this.something);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: Image(image: AssetImage(this.something))
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+}
