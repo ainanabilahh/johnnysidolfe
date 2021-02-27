2c99bc56decfb419882dfc537d61970fc6f05529
ainanabilahh <ainanabilahh@gmail.com>
2021-02-27 12:55:54 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -30,68 +30,159 @@ class _HomeScreenState extends State < HomeScreen > {
         // the App.build method, and use it to set our appbar title.
         title: Text("Johnny's Entertainment"),
       ),
-      body: Center(
-        child: GridView.count(
-          primary: false,
-          padding: const EdgeInsets.all(20),
-            crossAxisSpacing: 10,
-            mainAxisSpacing: 10,
-            crossAxisCount: 2,
-            children: < Widget > [
-              AspectRatio(
-                aspectRatio: 3 / 3,
-                child: GestureDetector(
-                  child: Image(image: AssetImage("images/arashi.jpg")),
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(
-                      builder: (context) => DetailScreen("images/arashi.jpg"),
-                    ));
-                  },
-                ),
+      body: GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 2,
+          children: < Widget > [
+            Container(
+              alignment: Alignment.topCenter,
+              decoration: BoxDecoration(
+                color: Colors.black,
               ),
-              GestureDetector(
-                child: Image(image: AssetImage("images/kinpuri.jpg")),
+              child: GestureDetector(
+                child: Column(children: < Widget > [
+                  Image(image: AssetImage("images/arashi.jpg")),
+                  Container(
+                    margin: const EdgeInsets.only(top: 25.0),
+                      child: Text(
+                        'Arashi',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20),
+                      )
+                  ),
+                ]),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) => DetailScreen("images/arashi.jpg"),
+                  ));
+                },
+              ),
+            ),
+            Container(
+              alignment: Alignment.topCenter,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: GestureDetector(
+                child: Column(children: < Widget > [
+                  Image(image: AssetImage("images/kinpuri.jpg")),
+                  Container(
+                    margin: const EdgeInsets.only(top: 25.0),
+                      child: Text(
+                        'King & Prince',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20),
+                      )
+                  ),
+                ]),
                 onTap: () {
                   Navigator.push(context, MaterialPageRoute(
                     builder: (context) => DetailScreen("images/kinpuri.jpg"),
                   ));
                 },
               ),
-              GestureDetector(
-                child: Image(image: AssetImage("images/sixtones.jpg")),
+            ),
+            Container(
+              alignment: Alignment.topCenter,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: GestureDetector(
+                child: Column(children: < Widget > [
+                  Image(image: AssetImage("images/sixtones.jpg")),
+                  Container(
+                    margin: const EdgeInsets.only(top: 25.0),
+                      child: Text(
+                        'SixTONES',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20),
+                      )
+                  ),
+                ]),
                 onTap: () {
                   Navigator.push(context, MaterialPageRoute(
                     builder: (context) => DetailScreen("images/sixtones.jpg"),
                   ));
                 },
               ),
-              GestureDetector(
-                child: Image(image: AssetImage("images/hsj.jpg")),
+            ),
+            Container(
+              alignment: Alignment.topCenter,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: GestureDetector(
+                child: Column(children: < Widget > [
+                  Image(image: AssetImage("images/hsj.jpg")),
+                  Container(
+                    margin: const EdgeInsets.only(top: 25.0),
+                      child: Text(
+                        'Hey! Say! JUMP',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20),
+                      )
+                  ),
+                ]),
                 onTap: () {
                   Navigator.push(context, MaterialPageRoute(
                     builder: (context) => DetailScreen("images/hsj.jpg"),
                   ));
                 },
               ),
-              GestureDetector(
-                child: Image(image: AssetImage("images/sexyzone.jpg")),
+            ),
+            Container(
+              alignment: Alignment.topCenter,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: GestureDetector(
+                child: Column(children: < Widget > [
+                  Image(image: AssetImage("images/sexyzone.jpg")),
+                  Container(
+                    margin: const EdgeInsets.only(top: 25.0),
+                      child: Text(
+                        'Sexy Zone',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20),
+                      )
+                  ),
+                ]),
                 onTap: () {
                   Navigator.push(context, MaterialPageRoute(
                     builder: (context) => DetailScreen("images/sexyzone.jpg"),
                   ));
                 },
               ),
-              GestureDetector(
-                child: Image(image: AssetImage("images/snowman.jpg")),
+            ),
+            Container(
+              alignment: Alignment.topCenter,
+              decoration: BoxDecoration(
+                color: Colors.black,
+              ),
+              child: GestureDetector(
+                child: Column(children: < Widget > [
+                  Image(image: AssetImage("images/snowman.jpg")),
+                  Container(
+                    margin: const EdgeInsets.only(top: 25.0),
+                      child: Text(
+                        'Snow Man',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20),
+                      )
+                  ),
+                ]),
                 onTap: () {
                   Navigator.push(context, MaterialPageRoute(
                     builder: (context) => DetailScreen("images/snowman.jpg"),
                   ));
                 },
               ),
-            ],
-        ),
-      )
+            ),
+          ],
+      ),
     );
   }
 }
